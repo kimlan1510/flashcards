3f6b93a7c9166e32defe157f077c47d888ffaeed
Jun Fritz and kimlan nguyen <kimlan@>
2017-04-25 10:20:01 -0700

    add jquery scripts, mains cards for flashcards.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+  $(".clickable1").click(function() {
+    $(".p1").slideToggle();
+  });
+  $(".clickable2").click(function() {
+    $(".p2").slideToggle();
+  });
+  $(".clickable3").click(function() {
+    $(".p3").slideToggle();
+  });
+  $(".clickable4").click(function() {
+    $(".p4").slideToggle();
+  });
+  $(".clickable5").click(function() {
+    $(".p5").slideToggle();
+  });
+});
